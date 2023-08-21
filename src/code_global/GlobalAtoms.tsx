5b8beeaf5440e84ea6f0eaffc3b079a8484242c2
fotomain <foto888999@gmail.com>
2023-08-21 09:40:11 +0300

    firebase9

--- src/code_global/GlobalAtoms.tsx
+++ src/code_global/GlobalAtoms.tsx
@@ -1,6 +1,7 @@
 
 
 import {Stack} from "@mui/material";
+
 import React from "react";
 
 export  const StackColumn = (props:any) =>{
@@ -20,3 +21,4 @@ export  const StackRow = (props:any) =>{
         </Stack>
     )
 }
+
